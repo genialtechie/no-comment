82a5820749084b7538ba80abd41e2da87556c928
genialtechie <thehaleembello@gmail.com>
2023-05-09 06:10:04 -0400

    insert comment

--- no-comment/src/extension.ts
+++ no-comment/src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import Authenticator from 'openai-token';
+import Authenticator from 'openai-token'; // eslint-disable-line
 import { ChatGPTUnofficialProxyAPI } from 'chatgpt';
 
 // This method is called when your extension is activated
@@ -11,19 +11,9 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "no-comment" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'no-comment.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from no-comment!');
-    }
+  context.subscriptions.push(
+    vscode.commands.registerCommand('no-comment.insertComment', insertComment)
   );
-
-  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
@@ -40,8 +30,35 @@ async function analyzeLine(line: string): Promise<string> {
     return response.text;
   } catch (error) {
     console.error('API call failed:', error);
-    throw error;
+    vscode.window.showErrorMessage('Failed to insert comment');
+    throw new Error('API call failed');
   }
 }
 
-async function insertComment() {}
+async function insertComment() {
+  try {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      vscode.window.showErrorMessage('No active editor');
+      throw new Error('No active editor');
+    }
+
+    const currentPos = editor.selection.active;
+    const currentLine = editor.document.lineAt(currentPos.line).text.trim();
+
+    if (!currentLine) {
+      vscode.window.showInformationMessage('No code on the current line');
+      throw new Error('No code on the current line');
+    }
+
+    const comment = await analyzeLine(currentLine);
+    editor.edit((editBuilder) => {
+      editBuilder.insert(currentPos, ` // ${comment}`);
+    });
+
+    vscode.window.showInformationMessage('Comment inserted');
+  } catch (error) {
+    console.error(error);
+    vscode.window.showErrorMessage('Failed to insert comment');
+  }
+}
